5d4021c4c292ef7d6221b15ffa083c240333306e
0xdead6eef <fake@mail.com>
2018-01-25 20:59:14 +0900

    change way of installing node.js

--- software_setup.sh
+++ software_setup.sh
@@ -24,14 +24,8 @@ fi
 #==============================================================================
 # setup nodejs8.9.4
 #==============================================================================
-mkdir -p $NODE_INST_DIR
-cd $NODE_INST_DIR
-wget https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-x64.tar.xz
-tar Jxf node-v8.9.4-linux-x64.tar.xz
-rm node-v8.9.4-linux-x64.tar.xz
-ln -s node-v8.9.4-linux-x64 node
-echo "export PATH=$NODE_INST_DIR/node/bin:$PATH" >> ~/.profile
-source ~/.profile
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 #==============================================================================
 # setup c++ compiler and libraries for building native library of ruby gems
